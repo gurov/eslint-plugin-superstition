175b8ec57a49d693845a798f1b5daa63b35536cd
gurov <lucius.gu@ya.ru>
2020-01-02 14:26:28 +0700

    Update must-have-8, improve a condition, task #1

--- rules/must-have-8.js
+++ rules/must-have-8.js
@@ -3,9 +3,9 @@
 const create = function (context) {
   return {
     Literal(node) {
-      const numbersAll = node.value.toString().match(new RegExp(/\d/, 'g'));
-      const numbers8 = node.value.toString().match(new RegExp(/8/, 'g'));
-      if (numbersAll && numbersAll.length > 0 && (numbers8.length < numbersAll.length / 2)) {
+      const numbersAll = node.value.toString().match(new RegExp(/\d/, 'g')) || [];
+      const numbers8 = node.value.toString().match(new RegExp(/8/, 'g')) || [];
+      if (numbersAll.length > 0 && (numbers8.length < numbersAll.length / 2)) {
         context.report({
           node,
           message: 'At least half of the numbers must be 8. Use more digits 8.'
